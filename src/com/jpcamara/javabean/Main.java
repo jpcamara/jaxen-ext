1c6007df46217c975c21e7b852172f72c18ade7e
JP Camara <johnpcamara@gmail.com>
2011-09-18 21:04:55 -0400

    more examples...which will go into tests shortly

--- src/com/jpcamara/javabean/Main.java
+++ src/com/jpcamara/javabean/Main.java
@@ -3,6 +3,9 @@ package com.jpcamara.javabean;
 import java.util.Map;
 import java.util.HashMap;
 
+import java.util.List;
+import java.util.Arrays;
+
 import com.jpcamara.javabean.EnhancedDocumentNavigator;
 import com.jpcamara.javabean.EnhancedJavaBeanXPath;
 
@@ -12,22 +15,35 @@ public class Main {
   public static void main(String[] args) throws Exception {
     Bean beany = new Bean();
     beany.mappy.put("key1", "value1");
+    beany.otherMappy.put("listKey", Arrays.asList("123", "323"));
+    beany.nestedMappy.put("nestedKey", Arrays.asList(
+      new HashMap<String, List<String>>() {{
+        put("key1", Arrays.asList("ok", "next"));
+      }},
+      new HashMap<String, List<String>>() {{
+        put("key2", Arrays.asList("further", "more"));
+      }}
+    ));
     beany.sweet = Boolean.TRUE;
     beany.awesomeness = Boolean.FALSE;
     
     EnhancedJavaBeanXPath path = new EnhancedJavaBeanXPath(".");
     Element root = (Element)path.selectSingleNode(beany);
-    System.out.println(root.getObject());
     
     EnhancedJavaBeanXPath primitiveBool = new EnhancedJavaBeanXPath("great");
     EnhancedJavaBeanXPath boxedBool = new EnhancedJavaBeanXPath("sweet");
     EnhancedJavaBeanXPath hasBool = new EnhancedJavaBeanXPath("awesomeness");
     EnhancedJavaBeanXPath mapValue = new EnhancedJavaBeanXPath("mappy/key1");
+    EnhancedJavaBeanXPath listValue = new EnhancedJavaBeanXPath("otherMappy/listKey[1]");
+    EnhancedJavaBeanXPath nested = new EnhancedJavaBeanXPath("nestedMappy/nestedKey[2]/key2[2]");
     
-    System.out.println(((Element)primitiveBool.selectSingleNode(root)).getObject());
+    System.out.println(root.getObject());
+    System.out.println(((Element)primitiveBool.selectSingleNode(root)).getObject() );
     System.out.println(((Element)boxedBool.selectSingleNode(root)).getObject());
     System.out.println(((Element)hasBool.selectSingleNode(root)).getObject());
     System.out.println(((Element)mapValue.selectSingleNode(root)).getObject());
+    System.out.println(((Element)listValue.selectSingleNode(root)).getObject());
+    System.out.println(((Element)nested.selectSingleNode(root)).getObject());
   }
   
   private static class Bean {
@@ -35,6 +51,9 @@ public class Main {
     private Boolean sweet;
     private Boolean awesomeness;
     private Map<String, String> mappy = new HashMap<String, String>();
+    private Map<String, List<String>> otherMappy = new HashMap<String, List<String>>();
+    private Map<String, List<HashMap<String, List<String>>>> nestedMappy = 
+      new HashMap<String, List<HashMap<String, List<String>>>>();
     
     public boolean isGreat() {
       return great;
@@ -50,6 +69,14 @@ public class Main {
     
     public Map<String, String> getMappy() {
       return mappy;
+    }
+    
+    public Map<String, List<String>> getOtherMappy() {
+      return otherMappy;
+    }
+    
+    public Map<String, List<HashMap<String, List<String>>>> getNestedMappy() {
+      return nestedMappy;
     }
   }
 }
